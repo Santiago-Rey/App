3445f514ec815e20d8da603d9b4b9ce5984fa164
Santiago Rey <dsrey9435@gmail.com>
2022-05-17 19:49:32 -0500

    Nuevas activitys, se creó el registro del botón empresarial, se puede seleccionar un producto, mejora visual

--- app/src/main/java/com/example/projectappcake/SelectionProduct.kt
+++ app/src/main/java/com/example/projectappcake/SelectionProduct.kt
@@ -2,11 +2,15 @@ package com.example.projectappcake
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class SelectionProduct : AppCompatActivity() {
+
+    lateinit var textCounts : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selection_product)
@@ -16,5 +20,14 @@ class SelectionProduct : AppCompatActivity() {
         val adapter = ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, items)
         drop.adapter = adapter
 
+        val btnBuy = findViewById<Button>(R.id.btnComprarF)
+        textCounts = findViewById(R.id.textC)
+        textCounts.setTextColor(resources.getColor(android.R.color.holo_red_dark))
+        btnBuy.setOnClickListener {
+            textCounts.text = "Compra Realizada!"
+        }
+
     }
+
+
 }
